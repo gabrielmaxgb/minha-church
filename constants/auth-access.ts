243cc7929a5db64740c136476b3d21583d935f80
gabrielmaxgb <gmaxgomes@gmail.com>
2026-07-16 02:56:40 -0300

    ++

--- constants/auth-access.ts
+++ constants/auth-access.ts
@@ -2,9 +2,9 @@
  * TEMP: desliga login/cadastro no site público até o go-live.
  * Reverter: AUTH_ACCESS_ENABLED = true
  */
-export const AUTH_ACCESS_ENABLED = false;
+export const AUTH_ACCESS_ENABLED = true;
 
 export const AUTH_PAUSED_TITLE = "Em breve";
 
 export const AUTH_PAUSED_MESSAGE =
-  "Login e cadastro estão temporariamente indisponíveis enquanto finalizamos o MinhaChurch. Volte em breve.";
+	"Login e cadastro estão temporariamente indisponíveis enquanto finalizamos o MinhaChurch. Volte em breve.";
